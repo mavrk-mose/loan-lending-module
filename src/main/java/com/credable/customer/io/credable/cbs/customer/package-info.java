2b573de7519b9d80eda49fad902ec9fc6a16422c
mavrk-mose <kippemoses25@gmail.com>
2023-02-09 16:52:27 +0300

    structured my classes in MVC architecture

--- src/main/java/com/credable/customer/io/credable/cbs/customer/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://credable.io/cbs/customer", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.credable.customer.io.credable.cbs.customer;
--- src/main/java/com/credable/transactions/io/credable/cbs/transaction/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://credable.io/cbs/transaction", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.credable.transactions.io.credable.cbs.transaction;
