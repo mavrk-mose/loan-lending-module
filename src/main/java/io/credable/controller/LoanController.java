1117387583b43a093e9bfffb43d9eeadca51ba07
Moses Kippe <53339961+mavrk-mose@users.noreply.github.com>
2023-03-03 14:18:57 +0000

    refactored parameters

--- src/main/java/io/credable/controller/LoanController.java
+++ src/main/java/io/credable/controller/LoanController.java
@@ -1,10 +1,11 @@
 package io.credable.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.credable.data.model.Loan;
+//import io.credable.data.model.Loan;
 import io.credable.data.model.QueryResponse;
 import io.credable.services.LoanService;
 
@@ -18,8 +19,8 @@ public class LoanController {
     }
 
     @GetMapping("/request-loan")
-    public QueryResponse createClient(@RequestBody Loan requestLoan) {
-        String customerNumber = requestLoan.getCustomerNumber();
+    public QueryResponse createClient(@RequestParam String customerNumber) {
+        //String customerNumber = requestLoan.getCustomerNumber();
         QueryResponse queryResponse = service.requestLoan(customerNumber);
         return queryResponse;    
     }
