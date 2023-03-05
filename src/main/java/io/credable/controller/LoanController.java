332ffdc80e825f26f0d5e063aa308060981c8170
Moses Kippe <53339961+mavrk-mose@users.noreply.github.com>
2023-03-05 08:35:17 +0000

    changed to PostMapping

--- src/main/java/io/credable/controller/LoanController.java
+++ src/main/java/io/credable/controller/LoanController.java
@@ -1,10 +1,10 @@
 package io.credable.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.credable.data.model.Loan;
 //import io.credable.data.model.Loan;
 import io.credable.data.model.QueryResponse;
 import io.credable.services.LoanService;
@@ -18,10 +18,11 @@ public class LoanController {
         this.service = service;
     }
 
-    @GetMapping("/request-loan/{customerNumber}")
-    public QueryResponse createClient(@PathVariable String customerNumber) {
-        //String customerNumber = requestLoan.getCustomerNumber();
+    @PostMapping("/request-loan")
+    public QueryResponse loanRequest(@RequestBody Loan requestLoan) {
+        String customerNumber = requestLoan.getCustomerNumber();
         QueryResponse queryResponse = service.requestLoan(customerNumber);
         return queryResponse;    
     }
+
 }
