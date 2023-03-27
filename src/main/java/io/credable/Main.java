286be0799f450ca21113d2b280b580795e73cb4b
mavrk-mose <kippemoses25@gmail.com>
2023-03-27 17:31:02 +0300

    added prometheus metrics

--- src/main/java/io/credable/Main.java
+++ src/main/java/io/credable/Main.java
@@ -2,7 +2,10 @@ package io.credable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+
 @SpringBootApplication
+@EnablePrometheusEndpoint
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
